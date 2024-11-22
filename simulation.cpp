c889c897565fce681c62b9f9d0c8d2722aac76b4
irenelega <105281326+irenelega@users.noreply.github.com>
2024-11-22 12:52:56 +0100

    Update simulation.cpp

--- simulation.cpp
+++ simulation.cpp
@@ -8,8 +8,8 @@
 #include <stdexcept>
 #include <iomanip>
 
-void check_parameters(SimulationParameters const& params) {
-    if (A_ <= 0 || B_ <= 0 || C_ <= 0 || D <= 0 || params.initial_x < 0 || params.initial_y < 0 || params.dt <= 0)
+void check_parameters(SimulationParameters const& parameters) {
+    if (A_ <= 0 || B_ <= 0 || C_ <= 0 || D <= 0 || parameters.initial_x < 0 || parameters.initial_y < 0 || parameters.dt <= 0)
     { // Checking for valid input parameters
     throw std::runtime_error{"Non valid parameters"};
     }
