02d0fbfd03fc9587a209d7c016f586170cf30ab7
irenelega <105281326+irenelega@users.noreply.github.com>
2024-11-22 12:25:36 +0100

    Update simulation.hpp

--- simulation.hpp
+++ simulation.hpp
@@ -9,7 +9,7 @@ struct Coordinates
   double X; //number of preys
   double Y; //number of predators
   double H; //integrale primo del moto
-  inline Coordinates operator / (const Coordinates &B) const 
+  inline Coordinates operator / (const Coordinates &B) const  //perchè solo con B???????
    {if (B.X == 0 || B.Y == 0) 
      {
       throw std::runtime_error{"Trying to divide by 0"};
@@ -17,22 +17,8 @@ struct Coordinates
     return Coordinates{X / B.X, Y / B.Y, t};
    }
 };
-  class Simulation {
-  const double A_;                // par 1
-  const double B_;                // par 2
-  const double C_;                // par 3
-  const double D_;                // par 4
-  std::vector<Coordinates> data_; // vector to save the evolution data
-public:
-  // Default constructor, ensures that t is set to 0
-  Simulation(double A, double B, double C, double D, Coordinates CI);
 
-  void evolve();
-  std::vector<Coordinates> run(double t);
-  Coordinates return_last_evolve();
-  std::vector<double> return_H();
-  std::vector<Coordinates> return_data();
-};
+
 
 
 
