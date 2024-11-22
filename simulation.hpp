72ea16a91f7e5863f8dc6aa9d9ca796232a389b4
irenelega <105281326+irenelega@users.noreply.github.com>
2024-11-22 12:28:16 +0100

    Update simulation.hpp

--- simulation.hpp
+++ simulation.hpp
@@ -9,7 +9,7 @@ struct Coordinates
   double X; //number of preys
   double Y; //number of predators
   double H; //integrale primo del moto
-  inline Coordinates operator / (const Coordinates &B) const  //perchè solo con B???????
+  inline Coordinates operator / (const Coordinates &B) const  //perchè solo con B??????? perchè B è un vettore?? e non cte
    {if (B.X == 0 || B.Y == 0) 
      {
       throw std::runtime_error{"Trying to divide by 0"};
