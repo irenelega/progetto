718788762180ec44df37ffeb12075fd90160d127
ariannabocchi <aribocchi02@gmail.com>
2024-11-22 12:35:30 +0100

    Update simulation.hpp

--- simulation.hpp
+++ simulation.hpp
@@ -14,9 +14,41 @@ struct Coordinates
      {
       throw std::runtime_error{"Trying to divide by 0"};
      }
-    return Coordinates{X / B.X, Y / B.Y, t};
+    return Coordinates{X / B.X, Y / B.Y, H};
    }
 };
+class Simulation {
+ private:
+  // x prede, y predatori
+  double current_x, current_y;
+
+  // A: tasso riproduzione prede
+  // B: tasso mortalità prede (che probabilità ha ogni predatore di uccidere
+  // una preda)
+  // C: tasso riproduzione predatori
+  // D: tasso mortalità predatori
+  double A, B, C, D;
+
+  // Valori di equilibrio
+  // (Quando il sistema è in equilibrio, dovrebbero stabilizzarsi a 1)
+
+  // Tempo globale e passo temporale della simulazione
+  double t;
+  double dt;
+
+ public:
+  Simulation(SimulationParameters const& params);
+
+  double get_relative_x() const;
+  double get_relative_y() const;
+  double get_current_x() const;
+  double get_current_y() const;
+  double get_H() const;
+
+  void evolve();
+  std::string print_info() const;
+};
+
 
 
 
