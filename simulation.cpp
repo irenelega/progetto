6b6e92782951e910988ea7fd74d20f3f17c23b7c
irenelega <105281326+irenelega@users.noreply.github.com>
2024-11-22 12:51:56 +0100

    Update simulation.cpp

--- simulation.cpp
+++ simulation.cpp
@@ -6,46 +6,15 @@
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
+#include <iomanip>
 
 void check_parameters(SimulationParameters const& params) {
-  if (params.initial_x < 0) {
-    std::cerr << "Il numero iniziale di prede deve essere >= 0" << std::endl;
-    throw std::invalid_argument("Invalid parameter value initial_x");
-  }
-
-  if (params.initial_y < 0) {
-    std::cerr << "Il numero iniziale di predatori deve essere >= 0"
-              << std::endl;
-    throw std::invalid_argument("Invalid parameter value initial_y");
-  }
-
-  if (params.A <= 0) {
-    std::cerr << "Il parametro A deve essere > 0" << std::endl;
-    throw std::invalid_argument("Invalid parameter value A");
-  }
-
-  if (params.B <= 0) {
-    std::cerr << "Il parametro B deve essere > 0" << std::endl;
-    throw std::invalid_argument("Invalid parameter value B");
-  }
-
-  if (params.C <= 0) {
-    std::cerr << "Il parametro C deve essere > 0" << std::endl;
-    throw std::invalid_argument("Invalid parameter value C");
-  }
-
-  if (params.D <= 0) {
-    std::cerr << "Il parametro D deve essere > 0" << std::endl;
-    throw std::invalid_argument("Invalid parameter value D");
-  }
-
-  if (params.dt <= 0) {
-    std::cerr << "Il passo temporale dt deve essere > 0" << params.dt
-              << std::endl;
-    throw std::invalid_argument("Invalid parameter value dt");
-  }
+    if (A_ <= 0 || B_ <= 0 || C_ <= 0 || D <= 0 || params.initial_x < 0 || params.initial_y < 0 || params.dt <= 0)
+    { // Checking for valid input parameters
+    throw std::runtime_error{"Non valid parameters"};
+    }
 }
 
 
-#include <cmath>
-#include <iomanip>
+
+
