fc32bde94e5aec4d22bdeb872864531d0aac5e96
irenelega <105281326+irenelega@users.noreply.github.com>
2024-12-01 18:51:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,60 +1,51 @@
-#include <iostream>
-int main()
-{
-  std::string userInput = user_input();
-  Simulation simulation(userInput);
+#include "simulation.cpp"
 
-  while (true) 
-    {
-      int command;
-      std::cout << "What do you want to do?\n
-        1- evolve to t+dt\n
-        2- run for selected runtime\n
-        3- return data vector\n
-        4- return current x, y\n
-        5- return H vector\n
-        6- stop\n << std::endl;              //CAMBIARE
-      std::cin >> command;
+int main() {
+   Simulation simulation{welcome()};
+   while (true) {
+    std::cout << "What do you want to do?\n"<<
+    "1-> Evolve populations to t+dt \n"<<
+    "2-> Run for selected runtime \n"
+    "3-> Return data vector \n"<<
+    "4-> Return current populations and X(t), Y(t), H \n"<< 
+    "5-> Stop simulation"
+    << std::endl;
+    int input;
+    std::cin >> input;
 
-        if (command == 1)
-      {
-        simulation.evolve();
-      } else if (command == 2)
-      {
-        std::cout << "For how long?" << std::endl;
-        double t;
-        std::cin >> t;
-        simulation.run(t);
-      } else if (command == 3)
-      {
-        std::vector<Coordinates> data = simulation.return_data();
-        std::cout << "X(t) and Y(t) estimations" << std::endl;
-        std::for_each(data.begin(), data.end(), [](const Coordinates &C) {print(C);});
-      } else if (command == 4)
-      {
-        Coordinates C = simulation.return_last_evolve();
-        std::cout << "X(t) and Y(t) estimations" << std::endl;
-        std::cout << "X(t)\t" << C.X << "\t Y(t) \t" << C.Y << "t = " << C.t << std::endl;
-      } else if (command == 5)
-      {
-        std::vector<double> data_H = simulation.return_H();
-        std::cout << "H constant of evolution" << std::endl;
-        std::for_each(data_H.begin(), data_H.end(), [](const double &H) { print(H); });
-      } else if (command == 6)
-      {
-        std::cout << "Bye bye" << std::endl;           //CAMBIARE
-        return 0;
-      } else
-      {
-        std::cout << "Invalid action. Please select again." << std::endl;    //CAMBIARE
-      }
-
-
-
-
-
-        
+    switch (input) {
+    case '1': {
+      simulation.evolve(); 
+      break;
     }
+    case '2': {
+      double t;
+      std::cout << "Insert run time " << std::endl;
+      std::cin >> t;
+      simulation.run(t);
+      break;
+    }
+    case '3': {
+      std::vector<Population> data = simulation.return_data();
+      std::cout << "X(t) and Y(t) estimations" << std::endl;
+      std::for_each(data.begin(), data.end(),
+                    [](const Population &P) { print(P); });
+      break;                
+    case '4': {
+      Population P{simulation.return_last_evolve()};
+      std::cout << "Preys and predators' estimated population" << std::endl;
+      std::cout << "X(t):\t" << P.x << "\nY(t):\t" << P.y <<"\nt = \t" << P.t << "\nH =\t " <<P.H<< std::endl;
+      break;              
+    }
+    case '5': {
+      std::cout << "Thank you, bye" << std::endl;
+      return 0;
+    }
+    default:
+      std::cout << "Invalid action. Please select again." << std::endl;
+    }
+  }
+}
 
   return 0;
 }
