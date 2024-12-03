4550b17351d07bfffdf0a58b9eb9139b787fc7a3
ariannabocchi <aribocchi02@gmail.com>
2024-12-03 18:18:51 +0100

    simulation.hpp

--- simulation.hpp
+++ simulation.hpp
@@ -9,6 +9,26 @@
 struct Population {
     double x, y, H;
     double t{0};
+    //operator / definition 
+   inline Population operator/(const Population &K) const 
+   {
+
+    // Verifica se almeno uno dei componenti di B è zero
+    if (K.x == 0) 
+    {
+        std::cerr << "Error:trying to divide by zero in the x component" <<std::endl;
+        throw std::runtime_error{"Errore: divisione per zero nella componente X."};
+    }
+    if (K.y == 0)
+    {   
+        std::cerr << "Error:trying to divide by zero in the y component" <<std::endl;
+
+        throw std::runtime_error{"Errore: divisione per zero nella componente Y."};
+    }
+    
+    
+    return Population{x / K.x, y / K.y, t};
+}
 };
 
 class Simulation {
@@ -18,8 +38,10 @@ class Simulation {
     double t;  // Tempo attuale
     std::vector<Population> data;  // Vettore che tiene traccia delle popolazioni evolute
 public:
+
     // Costruttore default
     Simulation(double A, double B, double C, double D, Population i_c);
+    
 
     // Metodi pubblici
     void evolve();  // Evolve la simulazione di un passo
@@ -39,9 +61,3 @@ public:
 
 
 
-
-
-
-
-
-#endif
