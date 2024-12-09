96352c7bb181d3ae1c473ccab31ee1b9a6741a7f
irenelega <105281326+irenelega@users.noreply.github.com>
2024-12-09 21:42:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include "simulation.cpp"
 
 int main() {
-   Simulation simulation=welcome();
-   while (true) {
-    std::cout << "What do you want to do?\n" <<
-    "1-> Evolve populations to t+dt \n" <<
-    "2-> Evolve populations for selected runtime \n" <<
-    "3-> Return current normalized populations x(t), y(t), and H, t \n" <<
-    "4-> Restart simulation and go back to initial conditions \n" <<
-    "5-> Stop simulation"
-    << std::endl;
+  Simulation simulation = welcome();
+  while (true) {
+    std::cout
+        << "What do you want to do?\n"
+        << "1-> Evolve populations to t+dt \n"
+        << "2-> Evolve populations for selected runtime \n"
+        << "3-> Return current normalized populations x(t), y(t), and H, t \n"
+        << "4-> Restart simulation and go back to initial conditions \n"
+        << "5-> Stop simulation" << std::endl;
     int input;
     std::cin >> input;
 
@@ -20,15 +20,19 @@ int main() {
     }
     case 2: {
       double t;
-      std::cout << "For how long do you want to evolve the system?: " << std::endl;
+      std::cout << "For how long do you want to evolve the system?: "
+                << std::endl;
       std::cin >> t;
       simulation.run(t);
       break;
     }
     case 3: {
-      Population P=simulation.take_last();
-      std::cout << "Preys and predators' last estimated population" << std::endl;
-      std::cout << "x_rel(" << P.t << ")=\t" << P.x << "\ny_rel(" << P.t << ")=\t" << P.y << "\nH =\t " << P.H << "\nt = \t" << P.t << std::endl;
+      Population P = simulation.take_last();
+      std::cout << "Preys and predators' last estimated population"
+                << std::endl;
+      std::cout << "x_rel(" << P.t << ")=\t" << P.x << "\ny_rel(" << P.t
+                << ")=\t" << P.y << "\nH =\t " << P.H << "\nt = \t" << P.t
+                << std::endl;
       break;
     }
     case 4: {
