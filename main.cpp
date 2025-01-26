d870670a7ef9f5c3334a17fafce1868f9c97842e
irenelega <105281326+irenelega@users.noreply.github.com>
2025-01-26 14:14:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
         << "What do you want to do?\n"
         << "1-> Evolve populations to t+dt \n"
         << "2-> Evolve populations for selected runtime \n"
-        << "3-> Return current normalized populations x(t), y(t), and H, t \n"
+        << "3-> Print current normalized populations x(t), y(t), and H, t \n"
         << "4-> Print each evolution's dt \n"
         << "5-> Restart simulation and go back to initial conditions \n"
         << "6-> Stop simulation" << std::endl;
